8428a3a95a6a6c877326e7808f035a1f9850a58a
Deu.K <370242479@qq.com>
2022-04-08 23:20:39 +0800

    Add DescriptorGroup
    Desc: Add DescriptorGroup descriptors count limit
    Type: Feature
    Link: NA

--- source/backend/dx12/DX12DescriptorGroup.cpp
+++ source/backend/dx12/DX12DescriptorGroup.cpp
@@ -30,20 +30,31 @@ void DX12DescriptorGroup::AddDescriptor(DescriptorType type,
     unsigned int id, unsigned int space, ShaderStage visibility)
 {
     CD3DX12_ROOT_PARAMETER parameter{};
+
     switch (type) {
     case DescriptorType::ConstantBuffer:
         parameter.InitAsConstantBufferView(id, space, ConvertShaderVisibility(visibility));
         break;
+
     case DescriptorType::StorageBuffer:
         // TODO
         break;
+
     case DescriptorType::ImageSampler:
         // TODO
         break;
+
     default:
         TI_LOG_RET_F(TAG, "Add descriptor information to descriptor group failed! "
             "Invalid descriptor type, only can be ConstantBuffer/StorageBuffer/ImageSampler.");
     }
+
+    if (descriptorCounter + 1 > description.descriptorCount) {
+        TI_LOG_W(TAG, "The number of descriptors overflowed, add descriptor information "
+            "to descriptor group success but you better check your logic and code!");
+    }
+    descriptorCounter++;
+
     parameters.emplace_back(parameter);
 }
 
@@ -52,9 +63,11 @@ void DX12DescriptorGroup::AddDescriptors(DescriptorType type,
 {
     if (beginId > endId) {
         TI_LOG_RET_F(TAG, "Add descriptor information to descriptor group failed! "
-            "Arguments are invalid, begin id > end id.");
+            "Arguments are invalid, failed because the begin id is large then the end id.");
     }
+
     CD3DX12_ROOT_PARAMETER parameter{};
+
     switch (type) {
     case DescriptorType::ConstantBuffer:
         descriptorRanges.emplace_back(std::make_unique<CD3DX12_DESCRIPTOR_RANGE>());
@@ -63,16 +76,26 @@ void DX12DescriptorGroup::AddDescriptors(DescriptorType type,
         parameter.InitAsDescriptorTable(1, descriptorRanges.back().get(),
             ConvertShaderVisibility(visibility));
         break;
+
     case DescriptorType::StorageBuffer:
         // TODO
         break;
+
     case DescriptorType::ImageSampler:
         // TODO
         break;
+
     default:
         TI_LOG_RET_F(TAG, "Add ranged descriptors information to descriptor group failed! "
             "Invalid descriptor type, only can be ConstantBuffer/StorageBuffer/ImageSampler.");
     }
+
+    if (descriptorCounter + (endId - beginId) > description.descriptorCount) {
+        TI_LOG_W(TAG, "The number of descriptors overflowed, add ranged descriptors information "
+            "to descriptor group success but you better check your logic and code!");
+    }
+    descriptorCounter += endId - beginId;
+
     parameters.emplace_back(parameter);
 }
 }
